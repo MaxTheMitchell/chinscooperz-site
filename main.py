8bad119be4d6857c4f0114ac2f873fd31d4b6c9b
MaxTheMitchell <rat2043@gmail.com>
2020-04-13 15:19:52 -0700

    add handlers class

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import http.server,socketserver,os
+from back_end.handlers import MyHandlers
 
 PORT = int(os.getenv("PORT"))
 
-class MyHandlers(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            return self.custom_get(None)
-        return super().do_GET()
-    
-    def custom_get(self,path):
-        self.send_response(200)
-        self.send_header("Content-type","text/html")
-        self.end_headers()
-        self.wfile.write(open('./front_end/static/html/index.html','rb').read())
-        return
-
-
 with socketserver.TCPServer(("",PORT),MyHandlers) as httpd:
-    print("on port localhost:{}".format(PORT))
+    print("on port {}".format(PORT))
     httpd.serve_forever()
