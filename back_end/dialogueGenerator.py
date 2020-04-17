d780b12bd20eda69a4cd6b377a04734d4e2cc932
MaxTheMitchell <rat2043@gmail.com>
2020-04-16 21:23:28 -0700

    add and impliment htmlFactory class

--- back_end/dialogueGenerator.py
+++ back_end/dialogueGenerator.py
@@ -7,18 +7,21 @@ class dialogueGenerator:
     def get_html(self,text):
         return reduce(
             lambda total,new: total + self.make_text_box(*new.strip().split('{')),
-            text.split('}')[0:-1],'')
+            text.split('}')[0:-1],''
+        )
             
     def make_text_box(self,name,text):
         return """
-            <table class="textbox" style="background-color: aqua;">
-            <tr>
-                <td>
-                    <img src="{}/{}.png">
-                <td>
-                <td>   
-                    <p>{}</p>
-                </td>
-            </tr>
+            <div class="textbox" style="background-color: aqua;">
+                <table>
+                <tr>
+                    <td>
+                        <img src="{}/{}.png">
+                    <td>
+                    <td>   
+                        <p>{}</p>
+                    </td>
+                </tr>
             </table>
+            </div>
             """.format(self.img_path,name,text)
