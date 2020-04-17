9758473725002eb450b5462939d1e0fbfd222eab
MaxTheMitchell <rat2043@gmail.com>
2020-04-17 13:17:54 -0700

    cleaned up imgs

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ PORT = int(os.getenv("PORT"))
 socketserver.TCPServer.allow_reuse_address = True
 
 with socketserver.TCPServer(("",PORT),MyHandlers) as httpd:
-    print("on port {}".format(PORT))
+    print("on port http://0.0.0.0:{}/".format(PORT))
     httpd.serve_forever()
